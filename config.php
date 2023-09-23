2b7937d2efd12a9bc291ddefd950671e6a902592
itsmostafa75 <122526754+itsmostafa75@users.noreply.github.com>
2023-09-23 20:57:46 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,8 @@
 <?php
 
 $subscription_urls = [
-  "http://104.168.107.230/aggregate.txt",
-  "http://77.91.84.45/subscribe.txt",
-  "https://alienvpn402.github.io/AlienVPN402-subscribe-servers/",
-  "https://branch.blanku.me/",
-  "https://raw.githubusercontent.com/irancpi/Subscription/main/Sub",
+  "https://reality1.elaheniloofar.link/0rtIq5SHzqWFvrgp0y58L/1ca8148d-5683-4886-bdb1-0fe14e4dad08/all.txt?name=sublink2-PSM&asn=PSM&mode=new",
+  "https://reality2.elaheniloofar.link/lGMxjUS7e09sBSExYvCzsq8/4ec4c1e4-b423-4d4d-b363-a046b74047b9/all.txt?name=sublink2-unknown&asn=unknown&mode=new",
+  "https://reality3.elaheniloofar.link/UMRPspkgNKi1r5niG/ef5ae00e-d05f-48c3-a6b7-0bc4fada0907/all.txt?name=reality3.elaheniloofar.link-PSM&asn=PSM&mode=new",
+  "https://reality4.elaheniloofar.link/Y8JJf1IvsadfMjqKVVg81h/ef5ae00e-d05f-48c3-a6b7-0bc4fada0907/all.txt?name=reality4.elaheniloofar.link-unknown&asn=unknown&mode=new"
 ];
